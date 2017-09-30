bf9a429b61a57c684d0d242c07ce2effe782da08
dajia9404 <869236596@qq.com>
2017-09-30 19:08:21 +0800

    201709301908

---
+++ UnityFrameWork/Assets/GameEngineEditor/GameComplied.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[InitializeOnLoad()]
+public class GameComplied
+{
+    static GameComplied()
+    {
+        EditorApplication.update += delegate()
+        {
+            if (EditorApplication.isCompiling)
+            {
+                if (EditorApplication.isPlaying)
+                {
+                    EditorApplication.isPlaying = false;
+                }
+            }
+        };
+    }
+}
